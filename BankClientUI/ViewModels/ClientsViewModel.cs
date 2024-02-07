b808c495d76bbd70d013943faa4e9c2deda1beb8
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-02-07 23:46:06 +0300

    ClientsView redesign and button activation state dependency on acces level added

--- BankClientUI/ViewModels/ClientsViewModel.cs
+++ BankClientUI/ViewModels/ClientsViewModel.cs
@@ -1,32 +1,23 @@
-﻿using System.Windows.Input;
-
-namespace BankClientUI.ViewModels
+﻿namespace BankClientUI.ViewModels
 {
-    //[QueryProperty(nameof(Worker), nameof(Worker))] to pass Interface instead of Class as Navigation parameter from LoginViewModel to this the possible soulution is to use IQuerryAttributable 
-    
     public partial class ClientsViewModel : BaseViewModel, IQueryAttributable
     {
         public ObservableCollection<ClientDetailsViewModel> Clients { get; set; }
-        public bool IsFullAccess => IsManager();
 
         [ObservableProperty]
-        private bool isBlocked = true;
-
-        [ObservableProperty]
-        private bool isBlockedPassport = true;
+        private bool isFullAccess;
 
         [ObservableProperty]
         private ClientDetailsViewModel? selectedClient = null;
 
         [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(IsFullAccess))]
         private IWorker? worker;
 
         private readonly IStorage storage;
 
         public ClientsViewModel(IStorage storage)
         {
-            Clients = new();
+            Clients = [];
             this.storage = storage;
             Title = "Clients List";
         }
@@ -35,17 +26,6 @@ namespace BankClientUI.ViewModels
         private void ShowDetails(ClientDetailsViewModel clientDetailsViewModel)
         {
             SelectedClient = clientDetailsViewModel;
-            IsBlocked = true;
-            IsBlockedPassport = true;
-        }
-
-        [RelayCommand]
-        private void CanEdit()
-        {
-            if (Worker is null) { return; }
-
-            IsBlocked = false;
-            IsBlockedPassport = !Worker.GetAccess().Equals("manager");
         }
 
         public void GetClients()
@@ -64,7 +44,13 @@ namespace BankClientUI.ViewModels
 
         public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
-            Worker = (IWorker)query[nameof(Worker)];
+            if (query.TryGetValue(nameof(Worker), out _))
+            {
+                Worker = (IWorker)query[nameof(Worker)];
+                IsFullAccess = IsManager();
+            }
+
+
         }
 
         private bool IsManager()
